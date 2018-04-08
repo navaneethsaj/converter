08300912b4541d8689ed4d6b1b71c1abd1c5cd25
Navaneeth <navaneethsaj@gmail.com>
2018-04-08 12:00:00 +0530

    launching now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const TIME_GAP = 10000;
+
 var express = require('express');
 var request = require('request');
 var admin = require('firebase-admin');
@@ -10,59 +12,69 @@ admin.initializeApp({
     databaseURL: "https://converter-13b02.firebaseio.com"
 });
 
-var db = admin.database();
-var ref = db.ref("/server");
+const app = express();
 
-//Checks if DB is outdated
-ref.on("value", function (snapshot) {
-    var obj = snapshot.val();
-    var timeNow = Date.now();
-    var timeDiff = timeNow - obj.lastUpdated ;
-    if (timeDiff < TIME_GAP) {
-        resp.send("Updated0 "+timeDiff);
-        dataBaseSender();
-    }else {
-        resp.write("Outdated0 "+timeDiff);
-        updater(timeNow)
+app.get('/', function (req, resp) {
+    console.log("Accessed");
+    //resp.send("Hello world");
+    var db = admin.database();
+    var ref = db.ref("/server");
+
+    //Checks if DB is outdated
+    ref.on("value", function (snapshot) {
+        var obj = snapshot.val();
+        var timeNow = Date.now();
+        var timeDiff = timeNow - obj.lastUpdated ;
+        if (timeDiff < TIME_GAP) {
+            resp.end("Updated0 "+timeDiff);
+            //dataBaseSender();
+        }else {
+            resp.end("Outdated0 "+timeDiff);
+            updater(timeNow)
+        }
+    });
+
+    function updater(timeNow) {
+        timeUpdater(timeNow);
+        //databaseUpdater();
     }
+
+    function timeUpdater(timeNow){
+        ref.child("lastUpdated").set(timeNow, function (error) {
+            if (error) {
+                //resp.end("error");
+            } else {
+                //resp.write("updatedNow :" + timeNow);
+                //resp.end();
+                //dataBaseSender();
+            }
+        });
+    }
+
+    /*function databaseUpdater(URL){
+        //Function to retrieve data and update DB
+
+        var url = "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
+            "key=d99803c970a04223998cabd90a741633" +
+            "&stop_id=it";
+
+        request({
+            url: URL,
+            json: true
+        }, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                console.log(body);
+                resp.send(body);// Print the json response
+            }
+        });
+    }*/
+
+    /*function dataBaseSender(){
+        //Function to send data back to CLIENT
+    }*/
+
 });
 
-function updater(timeNow) {
-    timeUpdater(timeNow);
-    //databaseUpdater();
-}
+app.listen(3000, () => console.log('http://localhost:3000'));
 
-function timeUpdater(timeNow){
-    ref.child("lastUpdated").set(timeNow, function (error) {
-        if (error) {
-            resp.end("error")
-        } else {
-            resp.write("updatedNow :" + timeNow);
-            resp.end();
-            dataBaseSender();
-        }
-    });
-}
-
-/*function databaseUpdater(URL){
-    //Function to retrieve data and update DB
-
-    var url = "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
-        "key=d99803c970a04223998cabd90a741633" +
-        "&stop_id=it";
-
-    request({
-        url: URL,
-        json: true
-    }, function (error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-            console.log(body);
-            resp.send(body);// Print the json response
-        }
-    });
-}*/
-
-/*function dataBaseSender(){
-    //Function to send data back to CLIENT
-}*/
