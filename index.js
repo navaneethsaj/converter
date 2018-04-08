c35c0934eef13a5bb7ed1f38b4a6699c936f092f
Navaneeth <navaneethsaj@gmail.com>
2018-04-08 22:22:45 +0530

    base and converter ready to launch

--- index.js
+++ index.js
@@ -97,5 +97,305 @@ app.get('/', function (req, resp) {
 
 });
 
+app.get('/convert', function (req, resp) {
+    console.log("\nAccessed : "+req.url);
+    //resp.send("Hello world");
+    var db = admin.database();
+    var ref = db.ref("/server");
+
+    if (!(req.query.from && req.query.to)) {
+        var responseObj = {
+            "success" : false,
+            "status" : "invalid argument type"
+        };
+        resp.end(JSON.stringify(responseObj));
+    }
+    else {
+        //Checks if DB is outdated
+        ref.once("value", function (snapshot) {
+            var obj = snapshot.val();
+            var timeNow = Date.now();
+            //console.log("timestamp :"+obj[DATAREF]["timestamp"]);
+            //console.log("timenow :"+timeNow);
+            var timeDiff = timeNow - obj["lastUpdated"];
+            //console.log("diff:"+timeDiff);
+            //console.log(timeDiff);
+            if (timeDiff < TIME_GAP) {
+                console.log("Up to date Data");
+                console.log("sending up to data , last updated (sec) : " + timeDiff / 1000 + ", saved data from DB ");
+
+                //CODE TO CONVERT
+                var rates = obj[DATAREF]["rates"];
+                var from = req.query.from.toUpperCase();
+                var to = req.query.to.toUpperCase();
+                //resp.end(from+" "+to);
+                //resp.end(JSON.stringify(rates));
+                if (from in rates && to in rates) {
+                    for (var key in rates) {
+                        if (rates.hasOwnProperty(key)) {
+                            rates[key] = rates[key] / rates[from];
+                        }
+                    }
+                    var responseObj = {
+                        "success": false,
+                        "status": "ok",
+                        "from": rates[from],
+                        "to": rates[to]
+                    };
+                    resp.end(JSON.stringify(responseObj));
+
+                } else {
+                    var responseObj = {
+                        "success": false,
+                        "status": "invalid currency type"
+                    };
+                    resp.end(JSON.stringify(responseObj));
+                }
+
+                //resp.end(JSON.stringify(obj[DATAREF]));
+
+
+                console.log("sending finished");
+                //dataBaseSender();
+            } else {
+                //resp.end("Outdated0 "+timeDiff);
+                //updater(timeNow);
+                console.log("Outdated DAta : trying to update");
+                timeUpdater(timeNow);
+                updateDBandSendJSON();
+            }
+
+            function updateDBandSendJSON() {
+                request({
+                    url: URL,
+                    json: true
+                }, function (error, response, body) {
+                    console.log("DataRetrieval from fixer successful");
+                    if (!error && response.statusCode === 200) {
+                        //console.log(body);
+                        ref.child(DATAREF).set(body);
+                        console.log("DatabaseUpdated: Ok");
+                        console.log("sending new data directly to client");
+
+                        //CODE TO CONVERT
+                        //CODE TO CONVERT
+                        var rates = body["rates"];
+                        var from = req.query.from.toUpperCase();
+                        var to = req.query.to.toUpperCase();
+                        //resp.end(from+" "+to);
+                        //resp.end(JSON.stringify(rates));
+                        if (from in rates && to in rates) {
+                            for (var key in rates) {
+                                if (rates.hasOwnProperty(key)) {
+                                    rates[key] = rates[key] / rates[from];
+                                }
+                            }
+                            responseObj = {
+                                "success": false,
+                                "status": "ok",
+                                "from": rates[from],
+                                "to": rates[to]
+                            };
+                            resp.end(JSON.stringify(responseObj));
+
+                        } else {
+                            responseObj = {
+                                "success": false,
+                                "status": "invalid currency type"
+                            };
+                            resp.end(JSON.stringify(responseObj));
+                        }
+                        //resp.end(JSON.stringify(body));// Print the json response
+
+                        console.log("datasend successful");
+                    }
+                });
+            }
+
+            function timeUpdater(time) {
+                ref.child("lastUpdated").set(time, function (error) {
+                    if (error) {
+                        //resp.end("error");
+                    } else {
+                        //resp.write("updatedNow :" + timeNow);
+                        //resp.end();
+                        //dataBaseSender();
+                    }
+                });
+            }
+        });
+        /*
+        function databaseUpdater(){
+            //Function to retrieve data and update DB
+
+            request({
+                url: URL,
+                json: true
+            }, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    //console.log(body);
+                    //resp.end(JSON.stringify(body));// Print the json response
+                }
+            });
+        }
+
+        /*function dataBaseSender(){
+            //Function to send data back to CLIENT
+        }*/
+    }
+
+}); //to implement convert any value
+
+app.get('/base', function (req, resp) {
+    console.log("\nAccessed : "+req.url);
+    //resp.send("Hello world");
+    var db = admin.database();
+    var ref = db.ref("/server");
+
+    if (!req.query.cur) {
+        var responseObj = {
+            "success" : false,
+            "status" : "invalid argument type"
+        };
+        resp.end(JSON.stringify(responseObj));
+    }
+    else {
+        //Checks if DB is outdated
+        ref.once("value", function (snapshot) {
+            var obj = snapshot.val();
+            var timeNow = Date.now();
+            //console.log("timestamp :"+obj[DATAREF]["timestamp"]);
+            //console.log("timenow :"+timeNow);
+            var timeDiff = timeNow - obj["lastUpdated"];
+            //console.log("diff:"+timeDiff);
+            //console.log(timeDiff);
+            if (timeDiff < TIME_GAP) {
+                console.log("Up to date Data");
+                console.log("sending up to data , last updated (sec) : " + timeDiff / 1000 + ", saved data from DB ");
+
+                //CODE TO CONVERT
+                var rates = obj[DATAREF]["rates"];
+                var base = req.query.cur.toUpperCase();
+
+                //resp.end(from+" "+to);
+                //resp.end(JSON.stringify(rates));
+                if (base in rates) {
+                    for (var key in rates) {
+                        if (rates.hasOwnProperty(key)) {
+                            rates[key] = rates[key] / rates[base];
+                        }
+                    }
+                    var responseObj = {
+                        "base": base,
+                        "date": obj[DATAREF]["date"],
+                        "rates": rates,
+                        "success": true,
+                        "timestamp": obj[DATAREF]["timestamo"]
+                    };
+                    resp.end(JSON.stringify(responseObj));
+
+                } else {
+                    var responseObj = {
+                        "success": false,
+                        "status": "invalid currency type"
+                    };
+                    resp.end(JSON.stringify(responseObj));
+                }
+
+                //resp.end(JSON.stringify(obj[DATAREF]));
+
+
+                console.log("sending finished");
+                //dataBaseSender();
+            } else {
+                //resp.end("Outdated0 "+timeDiff);
+                //updater(timeNow);
+                console.log("Outdated DAta : trying to update");
+                timeUpdater(timeNow);
+                updateDBandSendJSON();
+            }
+
+            function updateDBandSendJSON() {
+                request({
+                    url: URL,
+                    json: true
+                }, function (error, response, body) {
+                    console.log("DataRetrieval from fixer successful");
+                    if (!error && response.statusCode === 200) {
+                        //console.log(body);
+                        ref.child(DATAREF).set(body);
+                        console.log("DatabaseUpdated: Ok");
+                        console.log("sending new data directly to client");
+
+                        //CODE TO CONVERT
+                        var rates = obj[DATAREF]["rates"];
+                        var base = req.query.cur.toUpperCase();
+
+                        //resp.end(from+" "+to);
+                        //resp.end(JSON.stringify(rates));
+                        if (base in rates) {
+                            for (var key in rates) {
+                                if (rates.hasOwnProperty(key)) {
+                                    rates[key] = rates[key] / rates[base];
+                                }
+                            }
+                            var responseObj = {
+                                "base": base,
+                                "date": obj[DATAREF]["date"],
+                                "rates": rates,
+                                "success": true,
+                                "timestamp": obj[DATAREF]["timestamo"]
+                            };
+                            resp.end(JSON.stringify(responseObj));
+
+                        } else {
+                            var responseObj = {
+                                "success": false,
+                                "status": "invalid currency type"
+                            };
+                            resp.end(JSON.stringify(responseObj));
+                        }
+                        //resp.end(JSON.stringify(body));// Print the json response
+
+                        console.log("datasend successful");
+                    }
+                });
+            }
+
+            function timeUpdater(time) {
+                ref.child("lastUpdated").set(time, function (error) {
+                    if (error) {
+                        //resp.end("error");
+                    } else {
+                        //resp.write("updatedNow :" + timeNow);
+                        //resp.end();
+                        //dataBaseSender();
+                    }
+                });
+            }
+        });
+        /*
+        function databaseUpdater(){
+            //Function to retrieve data and update DB
+
+            request({
+                url: URL,
+                json: true
+            }, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    //console.log(body);
+                    //resp.end(JSON.stringify(body));// Print the json response
+                }
+            });
+        }
+
+        /*function dataBaseSender(){
+            //Function to send data back to CLIENT
+        }*/
+    }
+});
+
 app.listen(port, () => console.log('http://localhost:'+port));
 
